b2e6e2ef593ce9917bbe9c63c9c6a1b0ab22f2e7
Basel Elsrogy <baselhany88@gmail.com>
2026-08-28 22:06:47 +0300

    feat: add hero section with poster and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import Search from './components/Search';
 
 function App() {
-  return <div>App</div>;
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="/public/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
